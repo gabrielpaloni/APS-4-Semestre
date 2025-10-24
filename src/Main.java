0a4768c672b631e84a831c147b7f13f8046eb04e
gabrielpaloni <gabrielsbpaloni@gmail.com>
2025-10-24 09:13:57 -0300

    Mudanças no Main

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Opcional: Tenta aplicar um Look and Feel mais moderno (Nimbus)
         try {
             for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -14,10 +13,8 @@ public class Main {
                 }
             }
         } catch (Exception e) {
-            // Se não der, usa o padrão do Java
         }
 
-        // Inicia a primeira tela da aplicação
         java.awt.EventQueue.invokeLater(() -> {
             new TelaLogin().setVisible(true);
         });
